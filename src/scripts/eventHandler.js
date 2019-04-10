6a39ba972b830bb2e7fa95d18929f16ebda157bf
William Mitchell <wemitchell3@gmail.com>
2019-04-10 15:32:31 -0500

    Uploading post to api, clear dom, fetch all entries, and post to dom features.

--- src/scripts/eventHandler.js
+++ src/scripts/eventHandler.js
@@ -2,23 +2,28 @@ import api from "./apiManager"
 import postDOM from "./poiList"
 
 const eventHandler = {
-  handleClick () {
-    let nameValue = document.querySelector("#nameInput").value
-    let descriptionValue = document.querySelector("#descriptionInput").value
-    let costValue = document.querySelector("#costInput").value
-    let reviewValue = document.querySelector("#reviewInput").value
-    let locationValue = document.querySelector("#locationInput").value
+    handleClick() {
+        event.preventDefault()
+
+        let nameValue = document.querySelector("#nameInput").value
+        let descriptionValue = document.querySelector("#descriptionInput").value
+        let costValue = document.querySelector("#costInput").value
+        let reviewValue = document.querySelector("#reviewInput").value
+        let locationValue = document.querySelector("#locationInput").value
 
 
-    const poiEntry = {
-      name: nameValue,
-      description: descriptionValue,
-      cost: costValue,
-      review: reviewValue,
-      location: locationValue
-  }
-  api.postOnePOI(poiEntry).then(api.getAllPOIs).then(postDOM.renderPOIs)
-  }
+        const poiEntry = {
+            name: nameValue,
+            description: descriptionValue,
+            cost: costValue,
+            review: reviewValue,
+            location: locationValue
+        }
+        api.postOnePOI("interests", poiEntry)
+        .then(api.getAllPOIs("interests")
+            .then(postDOM.renderPOIs)
+        )
+    }
 }
 
 
